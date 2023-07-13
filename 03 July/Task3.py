c1ba8e61fb70ac360821ebe03e80322a95cfebc6
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-07-13 16:36:07 +0600

    Проверено дз от 03.07
    Комментарии к коду

--- 03 July/Task3.py
+++ 03 July/Task3.py
@@ -15,6 +15,10 @@ def divide_into_syllables(word):
         for i, letter in enumerate(word): 
             current_syllable += letter 
             
+            # VN: например, в слове "творог", последняя буква "г" не проходит проверку in vovels, а поскольку
+            # гласных больше нет, то эта буква выкидывается, и результат получается ['тво','ро'].
+            # Тут нужно ещё условие: если длина syllables уже равна количеству гласных, то оставшиеся буквы 
+            # просто приписываются к последнему слогу
             if letter.lower() in vowels: 
                 if i < len(word) - 1 and letter.lower() in sonorant_consonants and not word[i + 1].lower() in vowels: 
                     continue 
