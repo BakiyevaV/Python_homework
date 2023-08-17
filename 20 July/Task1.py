fb802735fab85cb0c91043ad4f15da1bc8eaadcd
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-08-17 12:23:43 +0600

    Проверено дз от 20.07
    Комментарии к коду

--- 20 July/Task1.py
+++ 20 July/Task1.py
@@ -10,8 +10,13 @@ def get_diag_words(input):
         for i in range(len(input)):
             if i == 0:
                 word2 += input[i][-1]
+                # VN: при i==0 ^^^^^^ эта строка
             else:
-                word2 += input[i][(len(input[i])-1) - i]
+                word2 += input[i][(len(input[i])-1) - i]  
+                # и эта ^^^^^^ строка - дают одинаковый результат, т.е. условие можно убрать.
+                # Ещё, учитывая, что в python индекс может быть отрицательным, выражение можно записать так:
+                # input[i][-i - 1]
+                # И можно word1 и word2 находить в одном и том же цикле
         sentence.append(word2)
         result = " ".join(sentence)
         return result
