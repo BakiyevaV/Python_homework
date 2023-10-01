9dfa7cb19ab22f97ab7e89a4d2f8b22a1581c4cc
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-10-01 12:39:29 +0600

    Проверено дз от 12.09
    Комментарии к коду

--- 12 Sep - Task1/Task1.py
+++ 12 Sep - Task1/Task1.py
@@ -9,11 +9,15 @@ def get_average(numbers):
     for num in numbers:
         sum += num
     global aver
+    #VN: Здорово, что вы не забыли про блокировку!
     lock.acquire()
     aver += sum / len(numbers)
     lock.release()
 
+#VN: Этот цикл лучше всего вынести в отдельную функцию с аргументами numbers и thread_num
 for i in range(thread_num):
+    #VN: вместо явной константы 10 длину среза для потоков лучше вычислять. Иначе ваше 
+    # решение очень сильно сильно зависит от конкретной длины numbers, и для другой длины будет падать
     start = i * 10
     stop = (i + 1) * 10
     thread = threading.Thread(target = get_average, args = [(numbers[start:stop])])
--- 12 Sep - Task2/Task2.py
+++ 12 Sep - Task2/Task2.py
@@ -5,21 +5,33 @@ def open_file(file_name,character):
         file.close()
     modify_text(text, character,file_name)
 
+#VN: первое, что бросилось в глаза - это нарушение принципа единой ответственности: функция open_file() не только 
+# занимается чтением файла, но и его модификацией. То же самое и с функцией modify_text() - она не только делает 
+# замены, но и записывает изменённый текст в файл. Вызовы в конце функции - это тоже часть функции!
+
+
+#VN: функция modify_text() - жуткий монстр, сложный для чтения и понимания. 
+# Разбивайте такие алгоритмы на более маленькие шаги - функции!
 
 def modify_text(text, character,file_name):
     signs = (",",".","!","?",":",'"',"»","«","'")
+    #VN: если в этом цикле ind переименовать в row, а второй цикл сделать for sign in signs:, всё станет понятнее
     for ind in range(len(text)):
         for i in range(len(signs)):
             new_line = text[ind].replace(signs[i],f" {signs[i]} ")
             text[ind] = new_line
     # print(text)
 
+    #VN: по сути происходит преобразование каждой строки в список слов. То же самое: станет сильно понятнее,
+    # если l_ind переименовать в row, а new_l - в word_list
     for l_ind in range(len(text)):
         new_l = text[l_ind].split()
         text[l_ind] = new_l
 
     # print(text)
     new_text = []
+    #VN: поскольку вы в результате этого блока формируете новый текст, можно обойтись без индексов.
+    # Т.е. for line in text:, for word in line:, new_word in character: и тд. Ч - читаемость!)))
     for l_ind in range(len(text)):
         for k in range(len(text[l_ind])):
             for j in range(len(character)):
@@ -29,12 +41,16 @@ def modify_text(text, character,file_name):
                     else: 
                         text[l_ind][k] = character[j][1].lower()
         new_line = " ".join(text[l_ind])
+        #VN: этот кусок тоже можно в отдельную функцию - по смыслу: "поправить знаки"
         for i in range(len(signs)):
             if signs[i] == "'":
                 new_line = new_line.replace(f"{signs[i]} ",f"{signs[i]}")
             new_line = new_line.replace(f" {signs[i]} ",f"{signs[i]} ")
             new_line = new_line.replace(f" {signs[i]}",f"{signs[i]}")
         write_text((f"{new_line}\n"),file_name)
+
+
+#VN: возможно, алгоритмы были бы чуть проще, если в open_file() читать сразу весь текст через read(), а не по строкам.
                 
 def write_text(line,file_name):
     with open(f"modified_{file_name}","a", encoding = 'utf-8') as file2:
@@ -45,6 +61,8 @@ files = {"Winnie_the_Pooh.txt":[("Винни-Пух", "Обжора"),("Винн
          "Gold_fish.txt":[("Старик", "Бедняга"),("Старика", "Беднягу"),("Старику", "Бедняге"),("Стариком", "Беднягой"),("Старике", "Бедняге"),("Старичок","Бедняга")],
          "Kolobok.txt":[("Колобок","Булка с корицей"),("Колобка","Булку с корицей"),("Колобку","Булке с корицей"),("Колобком","Булкой с корицей"),("Колобке","Булке с корицей")],
          "Tri_mushketera.txt":[("Артаньян","Лимонохват"),("Артаньяна","Лимонохвата"),("Артаньяну","Лимонохвату"),("Артаньяном","Лимонохватом"),("Артаньяне","Лимонохвате")]}
+
+#VN: этот цикл можно сделать ещё так: for file_name, replace_list in files.items():
 for key in files.keys():
     n = 1
     thread = threading.Thread(target = open_file, args = (key, files[key]))
