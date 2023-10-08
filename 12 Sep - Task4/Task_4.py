db3ca5b491266a957790e4d36bdf32aa210bea55
BakiyevaV <133982174+BakiyevaV@users.noreply.github.com>
2023-10-08 19:47:47 +0600

    added corrected files

--- 12 Sep - Task4/Task_4.py
+++ 12 Sep - Task4/Task_4.py
@@ -1,51 +1,45 @@
-from PyQt6.QtCore import QThread
-import PyQt6.QtWidgets as qt
+from PyQt6.QtCore import QThread, pyqtSignal
 from PyQt6.QtWidgets import QMainWindow, QPushButton,QLabel, QFileDialog
 from PyQt6 import uic
 
 class MyWindow(QMainWindow):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(MyWindow, cls).__new__(cls)
+        return cls.instance
+    
     def __init__(self):
         super().__init__()
         uic.loadUi("myui.ui",self)
+        self.button = self.findChild(QPushButton, "pushButton")
+        self.display = self.findChild(QLabel, "label")
+        self.button.clicked.connect(self.get_file)
 
-# class MyWidget(qt.QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         uic.loadUi("open_files.ui",self)
-    
+    def get_file (self):
+        fname, _ = QFileDialog.getOpenFileName(self, "Open File", "$HOME", "All Files (*);; Python Files (*.py);; PNG Files (*.png);; TXT Files (*.txt)")
+        self.textDisplayer = TextDisplayer(fname)
+        self.textDisplayer.textIsReady.connect(self.display_text)
+        self.textDisplayer.start()
+        
+    #Slot
+    def display_text(self,text):
+        self.display.setText(text)
+
 class TextDisplayer(QThread):
-    def __init__(self):
+    textIsReady = pyqtSignal(str)
+
+    def __init__(self, file_name):
         super().__init__()
-        self.window = MyWindow()
-        # self.files = MyWidget()
-        self.window.show()
+        self.file_name = file_name
 
     def run(self):
-        button = self.window.findChild(QPushButton, "pushButton")
-        print(button)
-        button.clicked.connect(self.open_file)
-        #VN: В этом месте ваш поток завершается, т.е. всё остальное происходит в главном потоке! В Task_3 то же самое.
-        # Решение, на самом деле, простое: все коннекты сигналов нужно делать в классе-виджете,
-        # а функция-слот на обработке нажатия кнопки: 
-        #   а) получает имя файла из QFileDialog, 
-        #   б) создаёт новый экземпляр класса-наследника QThread и передаёт ему всё что нужно для работы:
-        #      откуда прочитать данные и куда их поместить. А ещё лучше вместо "куда поместить" - передать
-        #      ссылку на метод, который новому потоку нужно будет вызвать после прочтения файла. Тогда будет
-        #      разделение ответственности: класс-виджет занимается интерфейсом, а класс-поток - получением данных.
-
-    def open_file (self):
-        fname, _ = QFileDialog.getOpenFileName(self.window, "Open File", "$HOME", "All Files (*);; Python Files (*.py);; PNG Files (*.png);; TXT Files (*.txt)")
-        # print(fname)
-        # print("Кнопка нажата")
-        with open (fname,"r",encoding="UTF-8") as file:
+        with open (self.file_name,"r",encoding="UTF-8") as file:
             text = file.read()
+            if text:
+               self.textIsReady.emit(text) 
             file.close()
-        print(text)
-        self.display_text(text)
+            
     
-    def display_text(self,text):
-        my_display = self.window.findChild(QLabel, "label")
-        my_display.setText(text)
 
 
 
--- 12 Sep - Task4/main.py
+++ 12 Sep - Task4/main.py
@@ -1,11 +1,11 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from Task_4 import TextDisplayer
+from Task_4 import MyWindow
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    text_displayer = TextDisplayer()
-    text_displayer.start()
+    window = MyWindow()
+    window.show()
     app.exec()
 
