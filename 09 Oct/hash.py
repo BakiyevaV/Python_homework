d3df1525d4f59bdcdd1abbed4384eccf93cceb70
BakiyevaV <133982174+BakiyevaV@users.noreply.github.com>
2023-10-20 09:33:31 +0600

    Added hw for 9 Oct

---
+++ 09 Oct/hash.py
@@ -0,0 +1,69 @@
+import hashlib
+import string
+import random
+# from logger import log
+import time
+def get_time(func):
+    def wrapper(self):
+        start_time = time.time()
+        print("Время начала ",start_time)
+        # log.log_i("Время начала ",start_time)
+        func(self)
+        end_time = time.time()
+        # log.log_i("Время окончания ",end_time)
+        print("Время окончания ",end_time)
+        res = end_time - start_time
+        # log.log_i(f"Результат: {res} секунд ({res//60} минут {res%60} секунд)")
+        print(f"Результат: {res} секунд ({res//60} минут {res%60} секунд)")
+        print("Результат ",res)
+    return wrapper
+
+class Hacker():
+    def __init__(self, sample, length, input_list):
+        self.sample = sample
+        self.length = length
+        self.input_list = input_list
+
+
+    @get_time
+    def hack_the_pass(self):
+        self.tries = 0
+        self.run = True
+        while self.run:
+            # log.log_d("Зашли в цикл")
+            variant= self.get_variant()
+            # log.log_i("Получили вариант ",variant)
+            result = self.hash_variant(variant)
+            # log.log_i("Получили результат ",result)
+            self.tries += 1
+            if (result == self.sample):
+                self.run = False
+                # log.log_t("Пароль: ", variant[0], "попыток: ", self.tries)
+                print("Пароль: ", variant[0], "попыток: ", self.tries)
+
+
+    def get_variant(self):
+        variant = ""
+        var_list = []
+        for _ in range(self.length):
+            sign = random.choice(self.input_list)
+            variant += sign
+        var_list.append(variant)
+        return var_list
+    
+    def hash_variant(self, variant):
+        varnt = variant[0]
+        # log.log_i("Вариант для хэширования ",varnt)
+        hash_object = hashlib.md5(varnt.encode("UTF-8"))
+        result = hash_object.hexdigest()
+        return result
+
+
+
+
+input_list = list(string.ascii_lowercase + string.ascii_uppercase + "0123456789" + "~!@#$%^&*()_+-=")
+
+h1 = Hacker("f232b847c44bd9992dd04efe19597ee6", 8, input_list)
+h1.hack_the_pass()
+
+
---
+++ 09 Oct/logger.py
@@ -0,0 +1,54 @@
+import datetime
+
+class Logger:
+    ERROR = 0
+    WARNING = 1
+    INFO = 2
+    DEBUG = 3
+    TRACE = 4
+    names = ["[E] ", "[W] ", "[I] ", "[D] ", "[T] "]
+
+    def __new__(cls, level, file=None, stdout=True):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Logger, cls).__new__(cls)
+            cls.instance.__init__(level, file, stdout)
+        return cls.instance
+    
+    def __init__(self, level, file=None, stdout=True):
+        self.stdout = stdout
+        self.file = file
+        self.current_log_level = level
+        
+
+    def set_log_level(self, level):
+        self.current_log_level = level
+
+
+    def log_prnt_write(self,color,lvl, *args, **kwargs):
+        # for k in kwargs:
+        #     val = kwargs[k]  
+        args_str = ' '.join(map(str, args))
+        if lvl <= self.current_log_level:
+            if self.stdout == True:
+                print(color,Logger.names[lvl], datetime.datetime.now(),*args,**kwargs )
+            if self.file != None: 
+                with open(f"{self.file}", "a", encoding='utf-8') as file:
+                    file.write(f"{Logger.names[lvl]}{datetime.datetime.now()}{args_str}\n")
+
+    def log_e(self,*args, **kwargs):
+        self.log_prnt_write("\033[31m",Logger.ERROR, *args, **kwargs)
+
+    def log_w(self,*args, **kwargs):
+        self.log_prnt_write("\033[33m",Logger.WARNING, *args, **kwargs)
+
+    def log_i(self,*args, **kwargs):
+        self.log_prnt_write("\033[32m",Logger.INFO, *args, **kwargs)
+
+    def log_d(self,*args, **kwargs):
+        self.log_prnt_write("\033[37m",Logger.DEBUG, *args, **kwargs)
+
+    def log_t(self,*args,**kwargs):
+        self.log_prnt_write("\033[34m",Logger.TRACE, *args, **kwargs)
+
+
+log = Logger(Logger.TRACE, file = "output.log",stdout = False)
